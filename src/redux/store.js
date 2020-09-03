21956274d263102c33c17d081275cdf9e40a5049
jctiru <jonathanchristiantiru@gmail.com>
2020-09-03 16:14:14 +1000

    Mount saga middleware on store before running sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,6 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-sagaMiddleware.run(rootSaga);
-
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
