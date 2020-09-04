94962e04d7215a5baff1754a06bc21df4c2afc03
jctiru <jonathanchristiantiru@gmail.com>
2020-09-04 16:48:07 +1000

    Create constants.js

---
+++ src/redux/constants.js
@@ -0,0 +1,12 @@
+export const ImageSize = Object.freeze({
+  COVER_SMALL: "cover_small",
+  SCREENSHOT_MED: "screenshot_med",
+  COVER_BIG: "cover_big",
+  LOGO_MED: "logo_med",
+  SCREENSHOT_BIG: "screenshot_big",
+  SCREENSHOT_HUGE: "screenshot_huge",
+  THUMB: "thumb",
+  MICRO: "micro",
+  s720p: "720p",
+  s1080p: "1080p",
+});
