c4ddd368fa830d695de344a4b9f0d2df2f5b81ba
jctiru <jonathanchristiantiru@gmail.com>
2020-09-06 22:02:35 +1000

    Add game column container components in home page

--- src/pages/home/home-page.component.jsx
+++ src/pages/home/home-page.component.jsx
@@ -8,6 +8,8 @@ import {
   getComingSoonStart,
 } from "../../redux/game/game.actions";
 import PopularNowContainer from "../../components/popular-now/popular-now.container";
+import GameCardColumnContainer from "../../components/game-card-column/game-card-column.container";
+import { GameColumnCategory } from "../../redux/constants";
 
 const HomePage = ({
   getPopularNowStart,
@@ -29,6 +31,26 @@ const HomePage = ({
         </div>
         <div>
           <PopularNowContainer />
+        </div>
+        <div className="row">
+          <div className="col-12 col-sm-4">
+            <h3>Recently Released</h3>
+            <GameCardColumnContainer
+              gameColumnCategory={GameColumnCategory.RECENTLY_RELEASED}
+            />
+          </div>
+          <div className="col-12 col-sm-4">
+            <h3>Coming Soon</h3>
+            <GameCardColumnContainer
+              gameColumnCategory={GameColumnCategory.COMING_SOON}
+            />
+          </div>
+          <div className="col-12 col-sm-4">
+            <h3>Most Anticipated</h3>
+            <GameCardColumnContainer
+              gameColumnCategory={GameColumnCategory.MOST_ANTICIPATED}
+            />
+          </div>
         </div>
       </div>
     </>
