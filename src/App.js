a3a03a90212838d001baffd09a34caed4eb64eca
jctiru <jonathanchristiantiru@gmail.com>
2020-09-07 14:43:27 +1000

    Create initial header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import Spinner from "./components/spinner/spinner.component";
 
 import "./App.css";
@@ -10,6 +11,7 @@ const HomePage = lazy(() => import("./pages/home/home-page.component"));
 const App = () => {
   return (
     <>
+      <Header />
       <Suspense fallback={<Spinner />}>
         <Switch>
           <Route exact path="/" component={HomePage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          GameWatch
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="navbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/news">
+                News
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
