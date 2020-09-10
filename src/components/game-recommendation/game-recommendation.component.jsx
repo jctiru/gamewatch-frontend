cc0a6c14bfd49c755e8a3af789cefb74297af4d2
jctiru <jonathanchristiantiru@gmail.com>
2020-09-10 15:44:41 +1000

    Create game recommendation component

---
+++ src/components/game-recommendation/game-recommendation.component.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+
+import { getModifiedImageUrl } from "../../redux/helper";
+import { ImageSize } from "../../redux/constants";
+
+SwiperCore.use([Navigation]);
+
+const GameRecommendation = ({ games }) => {
+  const swiperSettings = {
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    navigation: true,
+    loop: false,
+    breakpoints: {
+      425: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+  };
+
+  return (
+    <Swiper {...swiperSettings}>
+      {games.map((game) => (
+        <SwiperSlide key={game.id}>
+          <div>
+            <Link to={`${game.slug}`}>
+              <img
+                key={game.id}
+                className="img-fluid"
+                src={`${getModifiedImageUrl(
+                  game.cover.url,
+                  ImageSize.COVER_MED
+                )}`}
+                alt={`${game.name}`}
+              />
+            </Link>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default GameRecommendation;
