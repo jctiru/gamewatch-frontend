b2aa6f7d493dad4855e6220b5ebd7d0969f35ad7
jctiru <jonathanchristiantiru@gmail.com>
2020-09-12 15:49:31 +1000

    Create with-spinner-items component

---
+++ src/components/with-spinner-items/with-spinner-items.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import LoadingOverlay from "react-loading-overlay";
+
+import Spinner from "../spinner/spinner.component";
+
+const WithSpinnerItems = (WrappedComponent) => ({
+  isLoading,
+  areInitialItemsLoaded,
+  ...otherProps
+}) => {
+  return isLoading && !areInitialItemsLoaded ? (
+    <Spinner />
+  ) : (
+    <LoadingOverlay active={isLoading} spinner={<Spinner />}>
+      <WrappedComponent {...otherProps} />
+    </LoadingOverlay>
+  );
+};
+
+export default WithSpinnerItems;
