d8f1c8183c7d4d99027a8252d5b7badf84992401
jctiru <jonathanchristiantiru@gmail.com>
2020-09-13 11:56:27 +1000

    Change border style of about page card

--- src/pages/about/aboute-page.component.jsx
+++ src/pages/about/aboute-page.component.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 mx-auto">
-          <div className="card border-dark">
+          <div className="card border-secondary">
             <div className="card-body">
               <h2 className="card-title text-dark">About and Disclaimer</h2>
               <div className="card-text text-dark">
